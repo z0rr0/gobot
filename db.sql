636ce2fa7a520f470f3543b0dc2e9ba8b927a26e
Alexander Zaytsev <me@axv.email>
2022-04-06 18:27:28 +0300

    update sql schema

--- db.sql
+++ db.sql
@@ -1,18 +1,18 @@
 DROP TABLE IF EXISTS `chat`;
 CREATE TABLE IF NOT EXISTS `chat`
 (
-    `id`    VARCHAR(255) PRIMARY KEY NOT NULL,
-    `active`  SMALLINT     NOT NULL DEFAULT 0,
+    `id`      VARCHAR(255) PRIMARY KEY NOT NULL,
+    `active`  SMALLINT                 NOT NULL DEFAULT 0,
     `exclude` TEXT,
     `url`     TEXT,
-    `created` DATETIME     NOT NULL,
-    `updated` DATETIME     NOT NULL
+    `created` DATETIME                 NOT NULL,
+    `updated` DATETIME                 NOT NULL
 );
 
 /*
 id - unique chat identifier
-active - flat chat is active or not
-exclude - list of excluded members
+active - chat is active or not
+exclude - list of excluded users
 url - chat URL for calls
 created - timestamp of item create
 updated - timestamp of item update
