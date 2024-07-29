85695314064afcccb0de897ef119d57b44145484
Aleksandr Zaitsev <me@axv.email>
2024-07-29 09:33:13 +0300

    update gpt model

--- config/config.go
+++ config/config.go
@@ -79,7 +79,7 @@ func (gpt *GPT) Response(ctx context.Context, content string) (string, error) {
 	}
 
 	request := &aoapi.CompletionRequest{
-		Model:       aoapi.ModelGPT4o,
+		Model:       aoapi.ModelGPT4oMini,
 		Messages:    []aoapi.Message{{Role: aoapi.RoleUser, Content: content}},
 		MaxTokens:   gpt.MaxTokens,
 		Temperature: &gpt.Temperature,
